c168d4fbf032446d2c151bfde420eb381957502d
Emanuele Ianni <emanuele.ianni@maersk.com>
2019-07-06 14:46:46 +0200

    docker-compose

--- src/main/java/com/mercurius/poc/PocApplication.java
+++ src/main/java/com/mercurius/poc/PocApplication.java
@@ -2,12 +2,20 @@ package com.mercurius.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PocApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PocApplication.class, args);
-	}
+    @RequestMapping("/ping")
+    public String ping() {
+        return "Pong";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocApplication.class, args);
+    }
 
 }
